65f4698942cc6ffac8cf6004cbf35635171ac190
arafat717 <arafatjibon33@gmail.com>
2024-01-25 22:27:14 +0600

    added animation rotate

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,15 @@ import "./App.css";
 
 function App() {
   return (
+    // <div className="main">
+    //   <div className="box-1"></div>
+    //   <h1>This is first animation by Arafat Jibon</h1>
+    // </div>
     <div className="main">
-      <div className="box-1"></div>
-      <h1>This is first animation by Arafat Jibon</h1>
+      <div className="contianer-2">
+        <div className="box-1"></div>
+        <h1>arafat</h1>
+      </div>
     </div>
   );
 }
