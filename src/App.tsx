17feb876e93dda1e5cd4d50af8a3066adf8b7dac
arafat717 <arafatjibon33@gmail.com>
2024-01-25 20:20:06 +0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="main">
+      <div className="box-1"></div>
+      <h1>This is first animation by Arafat Jibon</h1>
+    </div>
+  );
+}
+
+export default App;
