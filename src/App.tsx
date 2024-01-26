23bada1214de5c30c2bb11ba225a30752c80f66d
arafat717 <arafatjibon33@gmail.com>
2024-01-26 22:04:43 +0600

    added loading animation

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,18 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="main">
-      <div className="box-1"></div>
-    </div>
+    // <div className="mains">
+    //   <div className="box-1"></div>
+    // </div>
     // <div className="main">
     //   <button className="btn">Hover Me</button>
     // </div>
+    <div className="main">
+      <div className="loading">
+        <div className="glass"></div>
+        <div className="box"></div>
+      </div>
+    </div>
   );
 }
 
