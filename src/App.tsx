f669cd68cc1334fdeb7caf025d6db3942e124ab5
arafat717 <arafatjibon33@gmail.com>
2024-01-26 21:03:48 +0600

    added css animation

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,12 @@ import "./App.css";
 
 function App() {
   return (
-    // <div className="main">
-    //   <div className="box-1"></div>
-    //   <h1>This is first animation by Arafat Jibon</h1>
-    // </div>
     <div className="main">
-      <button className="btn">Hover Me</button>
+      <div className="box-1"></div>
     </div>
+    // <div className="main">
+    //   <button className="btn">Hover Me</button>
+    // </div>
   );
 }
 
