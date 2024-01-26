c1fb8d2ddeaee15abd158108d429eb470316c2f2
arafat717 <arafatjibon33@gmail.com>
2024-01-26 22:30:36 +0600

    added css clip path transition

--- src/App.tsx
+++ src/App.tsx
@@ -8,10 +8,20 @@ function App() {
     // <div className="main">
     //   <button className="btn">Hover Me</button>
     // </div>
-    <div className="main">
-      <div className="loading">
-        <div className="glass"></div>
-        <div className="box"></div>
+    // <div className="main">
+    //   <div className="loading">
+    //     <div className="glass"></div>
+    //     <div className="box"></div>
+    //   </div>
+    // </div>
+    <div className="card-main">
+      <div className="card">
+        <h1>Container</h1>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quibusdam
+          iste repellendus saepe neque harum quos id assumenda culpa suscipit
+          sed.
+        </p>
       </div>
     </div>
   );
