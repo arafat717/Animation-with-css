5bc018fb6969fe30ce3476be953ec3a7ad228bd1
arafat717 <arafatjibon33@gmail.com>
2024-01-27 11:48:05 +0600

    added animation with javaScript

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,45 @@
 import "./App.css";
 
 function App() {
+  const name = "Arafat Hossen Joni";
+  const nameArr = name.split("");
   return (
-    // <div className="mains">
-    //   <div className="box-1"></div>
-    // </div>
-    // <div className="main">
-    //   <button className="btn">Hover Me</button>
-    // </div>
-    // <div className="main">
-    //   <div className="loading">
-    //     <div className="glass"></div>
-    //     <div className="box"></div>
-    //   </div>
-    // </div>
-    <div className="card-main">
-      <div className="card">
-        <h1>Container</h1>
-        <p>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quibusdam
-          iste repellendus saepe neque harum quos id assumenda culpa suscipit
-          sed.
-        </p>
+    <>
+      <div className="mains">
+        <div className="box-1"></div>
       </div>
-    </div>
+      <div className="main-btn">
+        <button className="btn">Hover Me</button>
+      </div>
+      <div className="main">
+        <div className="loading">
+          <div className="glass"></div>
+          <div className="box"></div>
+        </div>
+      </div>
+      <div className="card-main">
+        <div className="card">
+          <h1>Container</h1>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quibusdam
+            iste repellendus saepe neque harum quos id assumenda culpa suscipit
+            sed.
+          </p>
+        </div>
+      </div>
+      <div className="main-last">
+        <div className="container">
+          {nameArr.map((item, i) => (
+            <span
+              className="alphabat"
+              style={{ transitionDelay: `${i * 50}ms ` }}
+            >
+              {item}
+            </span>
+          ))}
+        </div>
+      </div>
+    </>
   );
 }
 
